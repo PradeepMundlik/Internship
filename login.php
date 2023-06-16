c09e1775b4c41e5716535261f0fb8ad6fc63e8e2
jvamshi36 <108867104+jvamshi36@users.noreply.github.com>
2023-06-16 09:44:22 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,7 +7,7 @@
 </head>
 <body>
 <?php
-    session_start();
+session_start();
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
     header('Location: welcome.php');
     exit;
@@ -17,20 +16,26 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
-$DATABASE_NAME = 'form';
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+$DATABASE_NAME_PATIENTS = 'form';
+$DATABASE_NAME_DOCTORS = 'form2';
+
+$con_patients = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME_PATIENTS);
+$con_doctors = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME_DOCTORS);
 
 if (mysqli_connect_error()) {
     exit('Error connecting to the database: ' . mysqli_connect_error());
 }
 
-if (isset($_POST['username'], $_POST['password'])) {
+if (isset($_POST['username'], $_POST['password'], $_POST['role'])) {
     if (empty($_POST['username']) || empty($_POST['password'])) {
         exit('Please fill both the username and password fields!');
     }
 
-    $stmt = $con->prepare('SELECT id, password FROM users WHERE username = ?');
-    $stmt->bind_param('s', $_POST['username']);
+    $con = $_POST['role'] === 'doctor' ? $con_doctors : $con_patients;
+    $database_name = $_POST['role'] === 'doctor' ? $DATABASE_NAME_DOCTORS : $DATABASE_NAME_PATIENTS;
+
+    $stmt = $con->prepare('SELECT id, password FROM users WHERE username = ? OR email = ?');
+    $stmt->bind_param('ss', $_POST['username'], $_POST['username']);
     $stmt->execute();
     $stmt->store_result();
 
@@ -42,27 +47,42 @@ if (isset($_POST['username'], $_POST['password'])) {
             $_SESSION['loggedin'] = true;
             $_SESSION['username'] = $_POST['username'];
             $_SESSION['id'] = $id;
-            header('Location: welcome.php');
+            $_SESSION['role'] = $_POST['role'];
+
+            if ($_POST['role'] === 'doctor') {
+                header('Location: welcome_doctor.php');
+            } elseif ($_POST['role'] === 'patient') {
+                header('Location: welcome_patient.php');
+            }
+
             exit;
         } else {
             echo 'Incorrect password!';
         }
     } else {
-        echo 'Incorrect username!';
+        echo 'Incorrect username or email!';
     }
 
     $stmt->close();
 }
 
-$con->close();
+$con_patients->close();
+$con_doctors->close();
 ?>
     <div class="register">
         <h1>Login</h1>
         <form action="login.php" method="post">
-            <label for="username">Username:</label>
+            <label for="username">Username or Email:</label>
             <input type="text" name="username" id="username" required>
             <label for="password">Password:</label>
             <input type="password" name="password" id="password" required>
+            <label>Role:</label>
+            <label for="role_patient">
+                <input type="radio" name="role" id="role_patient" value="patient" required> Patient
+            </label>
+            <label for="role_doctor">
+                <input type="radio" name="role" id="role_doctor" value="doctor" required> Doctor
+            </label>
             <input type="submit" value="Login">
             <p>Not yet registered? <a href="register.php">Register</a></p>
         </form>
