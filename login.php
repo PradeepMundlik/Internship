f26d72132dfbcc22b60994f20b0e412c5481d548
PradeepMundlik <pradeepmundlik@gmail.com>
2023-06-16 09:47:29 +0530

    some updates

--- login.php
+++ login.php
@@ -1,61 +1,67 @@
-
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <title>Login Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link href="style.css" rel="stylesheet" type="text/css">
 </head>
+
 <body>
-<?php
+    <?php
+
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+
     session_start();
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
-    header('Location: welcome.php');
-    exit;
-}
-
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'form';
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-
-if (mysqli_connect_error()) {
-    exit('Error connecting to the database: ' . mysqli_connect_error());
-}
-
-if (isset($_POST['username'], $_POST['password'])) {
-    if (empty($_POST['username']) || empty($_POST['password'])) {
-        exit('Please fill both the username and password fields!');
+    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
+        header('Location: welcome.php');
+        exit;
     }
 
-    $stmt = $con->prepare('SELECT id, password FROM users WHERE username = ?');
-    $stmt->bind_param('s', $_POST['username']);
-    $stmt->execute();
-    $stmt->store_result();
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_USER = 'pradeep';
+    $DATABASE_PASS = '12345678';
+    $DATABASE_NAME = 'kaustubha';
+    $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 
-    if ($stmt->num_rows > 0) {
-        $stmt->bind_result($id, $hashed_password);
-        $stmt->fetch();
+    if (mysqli_connect_error()) {
+        exit('Error connecting to the database: ' . mysqli_connect_error());
+    }
 
-        if (password_verify($_POST['password'], $hashed_password)) {
-            $_SESSION['loggedin'] = true;
-            $_SESSION['username'] = $_POST['username'];
-            $_SESSION['id'] = $id;
-            header('Location: welcome.php');
-            exit;
-        } else {
-            echo 'Incorrect password!';
+    if (isset($_POST['username'], $_POST['password'])) {
+        if (empty($_POST['username']) || empty($_POST['password'])) {
+            exit('Please fill both the username and password fields!');
         }
-    } else {
-        echo 'Incorrect username!';
+
+        $stmt = $con->prepare('SELECT id, password FROM users WHERE username = ?');
+        $stmt->bind_param('s', $_POST['username']);
+        $stmt->execute();
+        $stmt->store_result();
+
+        if ($stmt->num_rows > 0) {
+            $stmt->bind_result($id, $hashed_password);
+            $stmt->fetch();
+
+            if (password_verify($_POST['password'], $hashed_password)) {
+                $_SESSION['loggedin'] = true;
+                $_SESSION['username'] = $_POST['username'];
+                $_SESSION['id'] = $id;
+                header('Location: welcome.php');
+                exit;
+            } else {
+                echo 'Incorrect password!';
+            }
+        } else {
+            echo 'Incorrect username!';
+        }
+
+        $stmt->close();
     }
 
-    $stmt->close();
-}
-
-$con->close();
-?>
+    $con->close();
+    ?>
     <div class="register">
         <h1>Login</h1>
         <form action="login.php" method="post">
@@ -67,5 +73,8 @@ $con->close();
             <p>Not yet registered? <a href="register.php">Register</a></p>
         </form>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
+
 </html>
--- welcome.php
+++ welcome.php
@@ -1,4 +1,8 @@
 <?php
+
+error_reporting( E_ALL );
+ini_set( "display_errors", 1 ); 
+
 session_start();
 
 // Check if the user is not logged in
@@ -13,6 +17,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 <head>
     <meta charset="utf-8">
     <title>Welcome</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <style>
         body {
             background-color: #435165;
@@ -28,6 +33,8 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     <h1>Welcome, <?php echo $_SESSION['username']; ?>!</h1>
     <p>You have successfully logged in.</p>
     <a href="logout.php">Logout</a>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 </html>
 
